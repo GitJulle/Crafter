8aad55517cacac1954a791cf2405adfac272ee68
GitJulle <105446170+GitJulle@users.noreply.github.com>
2023-09-14 22:28:34 +0200

    Add files via upload

---
+++ Crafter/script.js
@@ -0,0 +1,35 @@
+var dirt = 0
+var bricks = 0
+var wood = 0
+
+
+function dirtadd() {
+    dirt += 1
+    document.getElementById('dirt counter').innerText = dirt + " dirt"
+}
+
+function woodadd() {
+    wood += 5
+    document.getElementById('wood counter').innerText = wood + " wood"
+}
+
+function combinedirt() {
+    if (dirt >= 100) {
+        dirt -= 100
+        bricks += 10
+        document.getElementById('dirt counter').innerText = dirt + " dirt"
+        document.getElementById('brick counter').innerText = bricks + " bricks"
+    }
+}
+
+function giveAxe() {
+    if ((dirt >= 50) && (bricks >= 25) )
+    {
+        dirt -= 50
+        bricks -= 25
+        document.getElementById('dirt counter').innerText = dirt + " dirt"
+        document.getElementById('brick counter').innerText = bricks + " bricks"
+        document.getElementById('tree image').innerHTML = '<img src="tree.png" alt="dirt" onclick="woodadd()" width="300px" height="300px">'
+        document.getElementById('AxeButton').remove()
+    }
+}
